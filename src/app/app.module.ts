aadd8e2afc1eddae6259f5073bf4987c0756981c
YacineTOUFIKi <yacine.toufiki@gmail.com>
2021-03-31 08:46:37 +0000

    FIRST COMMIT 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsNavBarComponent } from './components/products/products-nav-bar/products-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductsNavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/products/products-nav-bar/products-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-nav-bar',
+  templateUrl: './products-nav-bar.component.html',
+  styleUrls: ['./products-nav-bar.component.css']
+})
+export class ProductsNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
